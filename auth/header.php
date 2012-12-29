eec37cd30b392eef527921244e4d1b417561ecfa
Jakub Mikulas <jakub@jedenbod.cz>
2012-12-29 23:22:51 +0100

    EventLoop, styling

---
+++ auth/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>errfield - web error reporting and performance analysis</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="auth/css/style.css">
+</head>
+<body>
+    <div id="sidebar">
+        <div id="logo">
+            <a href="#">
+                <img src="auth/img/errfield_logo.jpg">
+            </a>
+        </div>
+        <ul>
+            <li><a href="#errors"><?php echo _('Errors'); ?></a></li>
+            <li><a href="#events"><?php echo _('Events'); ?></a></li>
+            <li><a href="#reports"><?php echo _('Reports'); ?></a></li>
+            <li><a href="#settings"><?php echo _('Settings'); ?></a></li>
+        </ul>
+    </div>
--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-
 	require_once 'config.php';
 
-	echo "<h1>Errors</h1>";
+	include_once 'auth/header.php';
 
-	$results = DB::query("SELECT type, data, meta,COUNT(*) as count FROM events GROUP BY data ORDER BY count DESC;");
-	foreach ($results as $row) {
-		echo "<h2 style=\"font-weight:400\">" . $row['type'] . ": \n";
-		echo "<strong> " . $row['data'] . "</strong> (".$row['count'] .")</h2>\n";
-		echo "Line: " . $row['meta'] . "<br />\n";
-		echo "-------------<br />\n";
-	}
+	include_once 'auth/eventLoop.php';
 
-
+	include_once 'auth/footer.php';
 ?>
