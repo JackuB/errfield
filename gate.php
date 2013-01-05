564158f6d70ff7d012c0da95c5f3688fb53e9361
Jakub Mikulas <jakub@jedenbod.cz>
2013-01-05 02:58:27 +0100

    Included Browsecap

--- gate.php
+++ gate.php
@@ -1,15 +1,38 @@
 <?php
 	require_once 'config.php';
 
+
+
+// Loads the class
+require 'Browsecap/Browscap.php';
+
+// The Browscap class is in the phpbrowscap namespace, so import it
+use phpbrowscap\Browscap;
+
+// Create a new Browscap object (loads or creates the cache)
+$bc = new Browscap('Browsecap/cache/');
+
+// Get information about the current browser's user agent
+$current_browser = $bc->getBrowser();
+
+// Output the result
+echo '<pre>'; // some formatting issues ;)
+print_r($current_browser);
+echo '</pre>';
+
+
 	if($_POST["type"] == "time") {
 		DB::insert('performance', array(
 			'time' => time(),
-		  	'elapsedtime' => htmlspecialchars($_POST["elapsedtime"]),
-		  	'url' => htmlspecialchars($_POST["url"]),
-		  	'useragent' => htmlspecialchars($_POST["ua"]),
-		  	'ip' => htmlspecialchars($_POST["ip"])
+			'browser' => htmlspecialchars($browser),
+		  	'redirectCount' => htmlspecialchars($_POST["redirectCount"]),
+		  	'redirectTime' => htmlspecialchars($_POST["redirectTime"]),
+		  	'requestTime' => htmlspecialchars($_POST["requestTime"]),
+		  	'responseTime' => htmlspecialchars($_POST["responseTime"]),
+		  	'domProcessingTime' => htmlspecialchars($_POST["domProcessingTime"]),
+		  	'domLoadingTime' => htmlspecialchars($_POST["domLoadingTime"]),
+		  	'loadEventTime' => htmlspecialchars($_POST["loadEventTime"])
 		));
-		echo "OK!";
 	} elseif($_POST["type"] != "") {
 		$text = htmlspecialchars($_POST["text"]);
 		$results = DB::query("SELECT * FROM events WHERE text='$text' AND state='ignored';");
@@ -31,13 +54,7 @@
 		  	'resolution' => htmlspecialchars($_POST["resolution"]),
 		  	'ip' => htmlspecialchars($_POST["ip"])
 		));
-		echo "OK! inserted to events";
-		echo $newState;
 	} else {
-		die('No error was POSTed');
+		die('Nothing was POSTed');
 	}
-
-
-$browser = get_browser();
-print_r($browser);
 ?>
