cb500c4fd9bae853c86d9658321802693a9c7fa0
Jakub Mikulas <jakub@jedenbod.cz>
2013-05-11 05:35:43 +0200

    UTF8 as default db

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ require_once 'libs/meekro/meekrodb.2.1.class.php';
 DB::$user = 'root';
 DB::$password = '';
 DB::$dbName = 'errfield';
+DB::$encoding = 'utf8'; // defaults to latin1 if omitted
 
 if(empty(DB::$dbName) and empty(DB::$user)) {
 	Echo "No database selected. Edit file config.php";
