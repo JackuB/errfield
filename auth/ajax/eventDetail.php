2d788486bc31a7de7867fa2716726c13c5cd55a2
Jakub Mikulas <jakub@jedenbod.cz>
2013-05-12 17:58:08 +0200

    Event detail - improved mysql handling

--- auth/ajax/eventDetail.php
+++ auth/ajax/eventDetail.php
@@ -12,13 +12,15 @@
 	$whatDBPerformance = "prj_" . $getProject[0]["table_name"] . "_performance";
 
 	$eventID = $_POST['eventId'];
-	$event = DB::query("SELECT *, COUNT(*) as count, MAX(time) AS lastoccurence FROM $whatDBEvents WHERE id=%i ORDER BY time ASC;",$eventID);
+	$eventText = DB::query("SELECT text FROM $whatDBEvents WHERE id=%i;",$eventID);
+
+	$event = DB::query("SELECT * FROM $whatDBEvents WHERE text=%s ORDER BY time DESC;",$eventText[0]['text']);
 ?>
 
 <div class="event detail">
     <div class="description" data-attr="<?php echo $event['id'] ?>">
     	<span class="eventMeta pull-left">
-            <?php echo _('occured '); if($event[0]['count'] == 1) {echo _('once');} else {echo $event[0]['count']; echo _(' times'); } ?> | <?php echo _('Last occurence'); ?>: <?php echo FormatTime($event[0]['lastoccurence']) ?>
+            <?php echo _('occured '); if(count($event) == 1) {echo _('once');} else {echo count($event); echo _(' times'); } ?> | <?php echo _('Last occurence'); ?>: <?php echo FormatTime($event[0]['time']) ?>
     	</span>
     	<span class="eventMeta pull-right">
     		<a data-project="<?=$whatProjectID?>" data-id="<?=$event[0]['id']?>" href="javascript:;">Ignore</a>&nbsp;&nbsp;&nbsp;<a data-project="<?=$whatProjectID?>" data-id="<?=$event[0]['id']?>" href="javascript:;">Solve</a>
@@ -30,9 +32,13 @@
     </div>
 </div>
 
+
 <div class="row-fluid">
 	<div class="span4">
-		<h2>Occurences</h2>
+		<h2>Occurences <small>last 7 days</small></h2>
+		<?php
+		$occurences = DB::query("SELECT * FROM $whatDBEvents WHERE text=%s AND from_unixtime(time) <= NOW() AND from_unixtime(time) >= DATE_SUB(from_unixtime(time), INTERVAL 7 DAY) ORDER BY time DESC;",$eventText[0]['text']);
+		?>
 	</div>
 	<div class="span4">
 		<h2>URLs</h2>
@@ -42,8 +48,9 @@
 	</div>
 </div>
 
+<?php var_dump($occurences); ?>
 
-<h2>Error detail</h2>
+<h2>Occurences</h2>
 
 <?php
 	if($event[0]['line'] != 0) {
