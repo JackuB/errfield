74c49df7a078eb0f6c350a044b99fe0ccc54f755
Jakub Mikulas <jakub@jedenbod.cz>
2013-05-12 18:11:06 +0200

    get_data function for curl

--- libs/functions.php
+++ libs/functions.php
@@ -82,6 +82,18 @@ function FormatTime($timestamp)
 
 
 
+
+function get_data($url) {
+  $ch = curl_init();
+  $timeout = 5;
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
 /*
   By Marco Arment <me@marco.org>.
   This code is released in the public domain.
