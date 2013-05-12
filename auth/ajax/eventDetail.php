30f3767cd71cf0f5034b47a59283192133b1a7bb
Jakub Mikulas <jakub@jedenbod.cz>
2013-05-12 18:19:27 +0200

    get data function

--- auth/ajax/eventDetail.php
+++ auth/ajax/eventDetail.php
@@ -48,31 +48,27 @@
 	</div>
 </div>
 
-<?php var_dump($occurences); ?>
+<?php /*var_dump($occurences); */?>
 
 <h2>Occurences</h2>
 
 <?php
 	if($event[0]['line'] != 0) {
-		$line = $event[0]['line'];
-		$lines = file($event[0]['file']);
-		if(isset($lines[$line+5])) {
-			echo "<pre class=\"brush: js; highlight: [".$event[0]['line']."]; toolbar: false; html-script: true; first-line: ".($line-4)."\">";
-			echo htmlspecialchars($lines[$line-5]);
-			echo htmlspecialchars($lines[$line-4]);
-			echo htmlspecialchars($lines[$line-3]);
-			echo htmlspecialchars($lines[$line-2]);
-			echo htmlspecialchars($lines[$line-1]);
-			echo htmlspecialchars($lines[$line]);
-			echo htmlspecialchars($lines[$line+1]);
-			echo htmlspecialchars($lines[$line+2]);
-			echo htmlspecialchars($lines[$line+3]);
-			echo htmlspecialchars($lines[$line+4]);
-			echo htmlspecialchars($lines[$line+5]);
-			echo "</pre>";
-		} else {
-			echo "<h3 class=\"well\">File has changed or couldn't be fetched</h3>";
-		}
+		echo "<pre class=\"brush: js; highlight: [".$event[0]['line']."]; toolbar: false; html-script: true; first-line: ".($line-4)."\">";
+		/*echo htmlspecialchars($lines[$line-5]);
+		echo htmlspecialchars($lines[$line-4]);
+		echo htmlspecialchars($lines[$line-3]);
+		echo htmlspecialchars($lines[$line-2]);
+		echo htmlspecialchars($lines[$line-1]);
+		echo htmlspecialchars($lines[$line]);
+		echo htmlspecialchars($lines[$line+1]);
+		echo htmlspecialchars($lines[$line+2]);
+		echo htmlspecialchars($lines[$line+3]);
+		echo htmlspecialchars($lines[$line+4]);
+		echo htmlspecialchars($lines[$line+5]);*/
+		echo get_data($event[0]['file'],$event[0]['line']);
+		echo htmlspecialchars($lines[222]);
+		echo "</pre>";
 	} else {
 		echo "<h3 class=\"well\">Looks like error happened in external JS file or there was error with reported line and/or url.</h3>";
 	}
--- libs/functions.php
+++ libs/functions.php
@@ -83,15 +83,19 @@ function FormatTime($timestamp)
 
 
 
-function get_data($url) {
+function get_data($url,$line) {
   $ch = curl_init();
-  $timeout = 5;
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-  $data = curl_exec($ch);
+  $timeout = 5; // set to zero for no timeout
+  curl_setopt ($ch, CURLOPT_URL, $url);
+  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+  $file_contents = curl_exec($ch);
   curl_close($ch);
-  return $data;
+  $lines = array();
+  $lines = explode("\n", $file_contents);
+  echo $lines[$line-1];
+  echo $lines[$line];
+  echo $lines[$line+1];
 }
 
 /*
