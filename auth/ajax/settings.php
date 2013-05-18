a85df1ff5d82592bd4c7464c5fe9652732a4e318
Jakub Mikulas <jakub@jedenbod.cz>
2013-05-18 17:27:10 +0200

    Settings page

--- auth/ajax/settings.php
+++ auth/ajax/settings.php
@@ -1,12 +1,20 @@
+<?php
+    require_once '../../config.php';
+?>
+
 <h1>Application settings</h1>
 
 <h2>Tracking code <em>(minified)</em></h2>
 <?php
-	echo "<textarea style=\"width:100%;\" rows=\"13\">";
+	echo "<textarea style=\"width:100%;font-family:consolas,monospace;\" rows=\"13\">";
+	echo "<!-- errfield error tracking -->";
+	echo "<script>";
 	echo htmlspecialchars(file_get_contents("./../../tracking_js/errfield.min.js"));
+	echo "<script>";
+	echo "<!-- /errfield error tracking -->";
 	echo "</textarea>";
 
-	echo "<pre class=\"brush: js; toolbar: false; html-script: true; syntaxhighlighter  javascript\">";
+	echo "<pre class=\"brush: js; toolbar: false; syntaxhighlighter: javascript\">";
 	echo htmlspecialchars(file_get_contents("./../../tracking_js/errfield.js"));
 	echo "</pre>";
 ?>
