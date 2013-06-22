def72ce073c55697fe9ef71913a13ed5fd38d32a
Jakub Mikulas <jakub@jedenbod.cz>
2013-06-22 12:45:37 +0200

    iPad compatible viewport

--- auth/template.php
+++ auth/template.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>errfield - web error reporting and performance analysis</title>
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=1024" />
     <link rel="stylesheet" href="auth/css/style.css">
     <script type="text/javascript" src="auth/js/head.load.min.js"></script>
 </head>
